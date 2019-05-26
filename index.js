491353167084c38b97464b5ce3d0597292118257
autonocat <45990205+autonocat@users.noreply.github.com>
2019-05-25 22:46:23 -0700

    v0.0.2

---
+++ index.js
@@ -0,0 +1,17 @@
+// Selects all divs and tunrs them GREEN
+// $("div").css("color", "#4BB178");
+
+// Selects all divs and tunrs BACKGROUND orange
+// $("div").css("background", "#FDBE50");
+
+// Selects ALL divs with CLASS of highlight and makes them 200px wide
+$(".highlight").css("width", "200px");
+
+// Selects div with id = third 
+$("#third").css("border", "2px solid black")
+
+// Selects FIRST DIV ONLY and changes the color
+$("div:first-of-type").css("color", "black")
+
+
+// Methods
